433c4d8ff45915fc66b3943f6f9498f3e995a7d6
Tanishk Yadav <112652811+tanishkcse@users.noreply.github.com>
2024-03-02 13:48:36 +0530

    Time: 2 ms (65.34%), Space: 41.6 MB (42.44%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -4,25 +4,12 @@ class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
         Arrays.sort(citations);
-
-        int[] arr = new int[n + 1];
-
-        for (int c : citations) {
-            if (c >= n) {
-                arr[n]++;
-            } else {
-                arr[c]++;
+        for(int i=0; i<n; i++){
+            int hIndex = n-i;
+            if(citations[i]>=hIndex){
+                return hIndex;
             }
         }
-
-        int count = 0;
-        for (int i = n; i >= 0; i--) {
-            count += arr[i];
-            if (count >= i) {
-                return i;
-            }
-        }
-
         return 0;
     }
 }
